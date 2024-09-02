ff63f0ea08f53cdfaa12bf31d540449f69f71515
Prince <ankitsharma0004d@gmail.com>
2024-09-03 02:25:09 +0530

    Added error files

---
+++ src/utils/error/app-error.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+    constructor(
+        name,
+        message,
+        explanation,
+        statusCode,
+    ){
+        super();
+        this.name = name;
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode =  statusCode
+    }
+}
