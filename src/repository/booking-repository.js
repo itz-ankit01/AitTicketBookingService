3368bd48da8c25fa9b1e8c29119b3cb8eccbea84
Prince <ankitsharma0004d@gmail.com>
2024-09-05 00:28:02 +0530

    Added create Booking api

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -5,6 +5,8 @@ const { StatusCodes } = require("http-status-codes");
 class BookingRepository {
   async create(data) {
     try {
+      const booking = await Booking.create(data);
+      return booking;
     } catch (error) {
       if (error.name == "SequelizeValidationError") {
         throw new ValidationError(error);
@@ -18,11 +20,21 @@ class BookingRepository {
     }
   }
 
-  async update(data){
+  async update(bookingId, data) {
     try {
-        
+      const booking = await Booking.findByPk(bookingId);
+      if(data.status){
+        booking.status = data.status;
+      }
+      await booking.save();
+      return booking;
     } catch (error) {
-        
+      throw new AppError(
+        "RepositoryError",
+        "Cannot update Booking",
+        "There was some issue updating the booking, please try again later",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
     }
   }
 }
